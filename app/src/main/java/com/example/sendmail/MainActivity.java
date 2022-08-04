d2ec7065a8a48fb4ebe705de51d3aed02622e26e
shiva <shivakushwaha8917@gmail.com>
2022-08-05 01:09:48 +0530

    initial git commit -m initial

---
+++ app/src/main/java/com/example/sendmail/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.sendmail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button send = (Button) this.findViewById(R.id.button1);
+
+        send.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Log.i("MainActivity", "Send Button Clicked.");
+
+                String fromEmail = "kushwaha143shiva@gmail.com";
+                String fromPassword = "nfozjglkyoeaqziy";
+                String toEmails = ((TextView) findViewById(R.id.editText3))
+                        .getText().toString();
+                List<String> toEmailList = Arrays.asList(toEmails
+                        .split("\\s*,\\s*"));
+                Log.i("MainActivity", "To List: " + toEmailList);
+                String emailSubject = ((TextView) findViewById(R.id.editText4))
+                        .getText().toString();
+                String emailBody = ((TextView) findViewById(R.id.editText5))
+                        .getText().toString();
+                String emailBody2 = emailBody + "Yes we can do" + " we can add this also";
+                new SendMailTask(MainActivity.this).execute(fromEmail,
+                        fromPassword, toEmailList, emailSubject, emailBody2);
+            }
+        });
+    }
+}
